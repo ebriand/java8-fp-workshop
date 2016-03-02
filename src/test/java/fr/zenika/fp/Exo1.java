0c8b7c675075f48cb58c514665e6c04ea066f617
Eric Briand <eric.briand@gmail.com>
2016-03-03 00:26:52 +0100

    Added solution for exo1

--- src/test/java/fr/zenika/fp/Exo1.java
+++ src/test/java/fr/zenika/fp/Exo1.java
@@ -3,15 +3,12 @@ package fr.zenika.fp;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
-import java.util.Optional;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import static java.util.Arrays.asList;
+import static java.util.Comparator.comparing;
+import static java.util.function.Function.identity;
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertThat;
 
@@ -42,7 +39,10 @@ public class Exo1 {
     @Test
     public void shouldFindEarthlings() {
         // TODO: Implement lambda to find earthlings
-        List<DBZCharacter> earthLings = dbzCharacters;
+        List<DBZCharacter> earthLings =
+                dbzCharacters.stream()
+                        .filter(c -> "Earth".equals(c.getHomePlanet()))
+                        .collect(Collectors.toList());
 
         assertThat(earthLings, hasItems(sonGohan, krillin));
         assertThat(earthLings, not(hasItems(sonGoku, vegeta, piccolo)));
@@ -50,33 +50,46 @@ public class Exo1 {
 
     @Test
     public void shouldBeOver9000() {
-        // TODO: Implement lambda to find who is over NINE THOUSANDS!!!!
-        List<DBZCharacter> powerfulCharacters = dbzCharacters;
+        // TODO: Implement lambda to find whose power is over NINE THOUSANDS!!!!
+        List<DBZCharacter> powerfulCharacters =
+                dbzCharacters.stream()
+                        .filter(c -> c.getPower() > 9000)
+                        .collect(Collectors.toList());
 
         assertThat(powerfulCharacters, hasItems(sonGoku, vegeta));
-        assertThat(powerfulCharacters, not(hasItems(krillin,sonGohan, piccolo)));
+        assertThat(powerfulCharacters, not(hasItems(krillin, sonGohan, piccolo)));
     }
 
     @Test
     public void shouldReturnNames() {
         // TODO: Implement lambda to extract names
-        List<String> characterNames = new ArrayList<>();
+        List<String> characterNames =
+                dbzCharacters.stream()
+                        .map(DBZCharacter::getName)
+                        .collect(Collectors.toList());
 
-        assertThat(characterNames, hasItems("Son Goku", "Son Gohan", "Piccolo", "Krillin", "Vegetas"));
+        assertThat(characterNames, hasItems("Son Goku", "Son Gohan", "Piccolo", "Krillin", "Vegeta"));
     }
 
     @Test
     public void shouldReturnNamesOfPowerfulCharacters() {
-        // TODO: Implement lambda to extract names of powerful characters
-        List<String> characterNames = new ArrayList<>();
+        // TODO: Implement lambda to extract names of powerful characters (over NINE THOUNSANDS!!!!!)
+        List<String> characterNames =
+                dbzCharacters.stream()
+                        .filter(c -> c.getPower() > 9000)
+                        .map(DBZCharacter::getName)
+                        .collect(Collectors.toList());
 
-        assertThat(characterNames, hasItems("Son Goku", "Vegetas"));
+        assertThat(characterNames, hasItems("Son Goku", "Vegeta"));
     }
 
     @Test
     public void shouldSortByPower() {
         // TODO: Implement lambda to sort by power
-        List<DBZCharacter> charactersSortedByPower = dbzCharacters;
+        List<DBZCharacter> charactersSortedByPower =
+                dbzCharacters.stream()
+                        .sorted(comparing(DBZCharacter::getPower).reversed())
+                        .collect(Collectors.toList());
 
         assertThat(charactersSortedByPower.get(0).getPower(), is(32000));
     }
@@ -84,15 +97,19 @@ public class Exo1 {
     @Test
     public void shouldReturnMaxPower() {
         // TODO: Implement lambda to return max power
-        int maxPower = 0;
+        int maxPower =
+                dbzCharacters.stream()
+                        .mapToInt(DBZCharacter::getPower)
+                        .max().getAsInt();
 
-        assertThat(maxPower, is(11250.2));
+        assertThat(maxPower, is(32000));
     }
 
     @Test
     public void shouldReturnNameOfMostPowerfulCharacter() {
         // TODO: Implement lambda to return the name of the most porwerful character
-        String mostPowerfulCharacter = "";
+        String mostPowerfulCharacter = dbzCharacters.stream()
+                .max(comparing(DBZCharacter::getPower)).get().getName();
 
         assertThat(mostPowerfulCharacter, is("Son Goku"));
     }
@@ -101,23 +118,31 @@ public class Exo1 {
     @Test
     public void shouldReturnAveragePower() {
         // TODO: Implement lambda to return average power
-        double averagePower = 0d;
+        double averagePower = dbzCharacters.stream()
+                .mapToInt(DBZCharacter::getPower)
+                .average().getAsDouble();
 
-        assertThat(averagePower, is(32000));
+        assertThat(averagePower, is(11250.2));
     }
 
     @Test
     public void shouldReturnNumberOfSaiyan() {
         // TODO: Implement lambda to return number of saiyan
-        int numberOfSaiyan = 0;
+        long numberOfSaiyan = dbzCharacters.stream()
+                .filter(c -> "Vegeta".equals(c.getHomePlanet()))
+                .count();
 
-        assertThat(numberOfSaiyan, is(2));
+        assertThat(numberOfSaiyan, is(2L));
     }
 
     @Test
-    public void shouldReturnCharacterNameWhenGivingSpecialMove() throws Exception {
+    public void shouldReturnCharacterNameWhoUseKamehameha() throws Exception {
         // TODO: Implement lambda to return character name when giving special move name
-        String characterWhoUseKamehameha = "Kamehameha";
+        String characterWhoUseKamehameha =
+                dbzCharacters.stream()
+                        .filter(c -> c.getSpecialMove().map("Kamehameha"::equals).orElse(false))
+                        .map(DBZCharacter::getName)
+                        .findFirst().get();
 
         assertThat(characterWhoUseKamehameha, is("Son Goku"));
     }
@@ -125,8 +150,14 @@ public class Exo1 {
     @Test
     public void shouldReturnSpecialMoveWhenGivingCharacterName() throws Exception {
         // TODO: Implement lambda to return special move name when giving character name
-        String sonGokuSpecialMove = "Son Goku";
-        String sonGohanSpecialMove = "Son Gohan";
+        String sonGokuSpecialMove =
+                dbzCharacters.stream()
+                        .filter(c -> "Son Goku".equals(c.getName()))
+                        .findFirst().get().getSpecialMove().map(identity()).orElse("");
+        String sonGohanSpecialMove =
+                dbzCharacters.stream()
+                        .filter(c -> "Son Gohan".equals(c.getName()))
+                        .findFirst().get().getSpecialMove().map(identity()).orElse("");
 
         assertThat(sonGokuSpecialMove, is("Kamehameha"));
         assertThat(sonGohanSpecialMove, is(""));
@@ -135,7 +166,11 @@ public class Exo1 {
     @Test
     public void shouldReturnSpecialMoves() throws Exception {
         // TODO: Implement lambda to return special move names
-        List<String> specialMoves = new ArrayList<>();
+        List<String> specialMoves =
+                dbzCharacters.stream()
+                        .map(DBZCharacter::getSpecialMove)
+                        .flatMap(speciaMove -> speciaMove.isPresent() ? Stream.of(speciaMove.get()) : Stream.empty())
+                        .collect(Collectors.toList());
 
         assertThat(specialMoves, hasItems("Kamehameha", "Special Beam Cannon", "Final Flash"));
     }
